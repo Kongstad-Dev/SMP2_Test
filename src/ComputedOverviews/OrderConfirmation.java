50cb83ae107602efcb08849087078a8de72f2633
Olarsen <Oliver1703dk@hotmail.com>
2023-04-17 12:47:07 +0200

    Dunno man

--- src/ComputedOverviews/OrderConfirmation.java
+++ src/ComputedOverviews/OrderConfirmation.java
@@ -16,17 +16,13 @@ public class OrderConfirmation {
 
 
 
-    // TODO: Delete....
-//    String color;
-//    BigDecimal price;
-//    String str;
-
-
 
     BigDecimal amountSpent;
     BigDecimal totalTaxes;
+
+
+    // TODO: Placeholder String
     String productVisualization =  "Picture of product from DAM";
-    // TODO: ??? ProductQuantity is pr product, dunno if it should be here
 
     LocalDate timeOfArrival;
 
@@ -85,6 +81,11 @@ public class OrderConfirmation {
 
 
 
+
+    public void displayProductInformation(){
+        // TODO: Code to display all the product information
+
+    }
 
     public void convertToPDF(){
         // TODO: Code to convert it to PDF
